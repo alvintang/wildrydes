04507fe6c9f8553d9e22b8c963844e4e43d57980
alvintang <alvintang@gmail.com>
2019-07-22 13:35:36 +0800

    added cognito configs

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_9MRxLcUoG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5mdil887o6268oeleb32lktmgo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
